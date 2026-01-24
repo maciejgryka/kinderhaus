b8ea0bd52bdf42c7f4174dee3d68b1a1b3ce5a19
Maciej Gryka <maciej@distillabs.ai>
2026-01-24 23:04:42 +0100

    add gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://maciejgryka.github.io',
+  base: '/kinderhaus',
   vite: {
     plugins: [tailwindcss()]
   }
